afb145768c14187da0aba98f8b72e02aa5054b26
Sebastien Dubois <sdubois@protonmail.ch>
2015-04-06 02:46:32 +0300

    cleanup + convert to multimodule --> still works. But if I put the submodule's config in the root build.gradle under subprojects { ... },. fails with 'plugin aspectj not found'!

--- submodule/src/main/java/com/hello/MyClass.java
+++ submodule/src/main/java/com/hello/MyClass.java
@@ -1,12 +1,13 @@
 package com.hello;
 
 public class MyClass {
-    public void foo (int number, String name) {
-        System.out.println("Inside foo (int, String) >>> number : " + number + ", name : " + name);
-    }
 
     public static void main(String[] args) {
         MyClass myObject = new MyClass();
         myObject.foo(1, "Russ Miles");
+    }
+
+    public void foo (int number, String name) {
+        System.out.println("Inside foo (int, String) >>> number : " + number + ", name : " + name);
     }
 }
